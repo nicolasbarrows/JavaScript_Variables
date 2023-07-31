921c1f8cfd3244a4b23c2398de24a7ea6fec64f0
Nicolas Barrows <contact@nicolasbarrows.com>
2023-07-31 18:47:13 -0500

    Excercise 1-5 COMPLETE

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Nicolas";
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Barrows";
+age = 27;
+
+console.log(firstName + " " + lastName + ", age " + age);
+
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(language + " was first released in " + createdYear + ".");
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log("Size " + selectedSize + " $" + price + " (" + salePercentage + "% OFF). Only " + stock + " left in stock!");
+
+// Exercise 5;
+let title = "Name of the Wind";
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title + " by " + author + " || " + bookmark + "/" + pageCount + " read");
